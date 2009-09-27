d4846a34025d46045ca3c18e90fefe13c691b3f6
Jan Dudek <jd@jandudek.com>
2009-09-27 17:24:44 +0200

    refactoring, custom regexps for arguments

--- Router.php
+++ Router.php
@@ -1,12 +1,5 @@
 <?php
 
-/*
-
-TODO
- * filtry
- * wlasne re argumentow
-*/
-
 class Johnny_Router
 {
 	/**
@@ -14,49 +7,63 @@ class Johnny_Router
 	 * array(
 	 *   [0] => array(
 	 *     'pattern' =>
-	 *     'args' => array(
-	 *       ['xyz'] => array(re => ... | const => ...)
-	 *     )
+	 *     'vars' => array()
+	 *     'consts' => array()
 	 *     'options' => array()
+	 *     're' => 
 	 *   )
 	 * )
 	 */
 	protected $routes = array();
 	
-	protected $argNameRe = '/\:([a-zA-Z0-9_]+)/';
+	protected $varNameRe = '/\:([a-zA-Z0-9_]+)/';
 	
-	protected $defaultArgRe = '.*?';
+	protected $defaultVarRe = '.*?';
 
-	public function connect($pattern, $args, $options = array())
+	public function connect($pattern, $args = array(), $options = array())
 	{
-		$routeArgs = array();
-		preg_match_all($this->argNameRe, $pattern, $matches);
-		foreach ($matches[1] as $argName) {
-			$routeArgs[$argName] = array('re' => $this->defaultArgRe);
-		}
-		foreach ($args as $k => $v) {
-			if (is_array($v)) {
-				$routeArgs[$k] = $v;
+		$vars = array();
+		preg_match_all($this->varNameRe, $pattern, $matches);
+		foreach ($matches[1] as $name) {
+			if (isset($args[$name])) {
+				$vars[$name] = $args[$name];
 			} else {
-				$routeArgs[$k] = array('const' => $v);
+				$vars[$name] = $this->defaultVarRe;
 			}
 		}
-		array_push($this->routes, array('pattern' => $pattern, 'args' => $routeArgs, 'options' => $options));
+		
+		$consts = array();
+		foreach ($args as $k => $v) {
+			if (!isset($vars[$k])) {
+				$consts[$k] = $v;
+			}
+		}
+		
+		$routeRe = "#^$pattern$#";
+		$sortedVars = $vars;
+		krsort($sortedVars);
+		foreach ($sortedVars as $name => $varRe) {
+			$routeRe = str_replace(":$name", "($varRe)", $routeRe);
+		}
+		
+		array_push($this->routes, array(
+			'pattern' => $pattern,
+			'vars' => $vars,
+			'consts' => $consts,
+			're' => $routeRe,
+			'options' => $options
+		));
 	}
 	
 	public function match($request)
 	{
 		foreach ($this->routes as $route) {
-			$re = '#^' . preg_replace($this->argNameRe, '(.*?)', $route['pattern']) . '$#';
-			if (preg_match($re, $request, $matches)) {
-				$result = array();
-				foreach ($route['args'] as $k => $v) {
-					if (isset($v['const'])) $result[$k] = $v['const'];
-				}
-				$varArgs = $this->getVarArgs($route);
-				if (count($varArgs) > 0) {
-					array_shift($matches);
-					$result = array_merge($result, array_combine($varArgs, $matches));
+			if (preg_match($route['re'], $request, $matches)) {
+				$result = $route['consts'];
+				$i = 1;
+				foreach ($route['vars'] as $name => $re) {
+					$result[$name] = $matches[$i];
+					$i += 1 + substr_count($re, '(');
 				}
 				return $result;
 			}
@@ -66,7 +73,7 @@ class Johnny_Router
 	public function url($args)
 	{
 		foreach ($this->routes as $route) {
-			if ($this->matchArgs($route['args'], $args)) {
+			if ($this->matchArgs($route, $args)) {
 				$result = $route['pattern'];
 				foreach ($args as $k => $v) {
 					$result = str_replace(':'.$k, $v, $result);
@@ -76,20 +83,16 @@ class Johnny_Router
 		}
 	}
 	
-	protected function matchArgs($routeArgs, $givenArgs) {
-		foreach ($routeArgs as $k => $v) {
+	protected function matchArgs($route, $givenArgs) {
+		foreach ($route['consts'] as $k => $v) {
+			if (!isset($givenArgs[$k]) || $givenArgs[$k] != $v) return false;
+			unset($givenArgs[$k]);
+		}
+		foreach ($route['vars'] as $k => $v) {
 			if (!isset($givenArgs[$k])) return false;
-			if (isset($v['const']) && $v['const'] != $givenArgs[$k]) return false;
+			if ($v != $this->defaultVarRe && !preg_match("#$v#", $givenArgs[$k])) return false;
 			unset($givenArgs[$k]);
 		}
 		return empty($givenArgs);
-	}
-	
-	protected function getVarArgs($route) {
-		$a = array();
-		foreach ($route['args'] as $name => $arg) {
-			if (!empty($arg['var'])) $a[] = $name;
-		}
-		return $a;
 	}
 }
--- RouterTest.php
+++ RouterTest.php
@@ -7,31 +7,38 @@ class RouterTest extends PHPUnit_Framework_TestCase
 {
 	public function testMatch() {
 		$r = new Johnny_Router();
-		$r->connect('/news', array('action' => 'NewsList'));
-		$r->connect('/news/:id', array('action' => 'NewsShow'));
+		$r->connect('/news', array('action' => 'list'));
+		$r->connect('/news/:id', array('action' => 'show'));
 
-		$this->assertEquals($r->match('/news'), array('action' => 'NewsList'));
-		$this->assertEquals($r->match('/news/12'), array('action' => 'NewsShow', 'id' => 12));
-	}
-
-	public function testMatchWithRe() {
-		$r = new Johnny_Router();
-		$r->connect('/news', array('action' => 'NewsList'));
-		$r->connect('/news/:id', array('action' => 'NewsShow', 'id' => array('re' =>'\d+')));
-
-		$this->assertEquals($r->match('/news'), array('action' => 'NewsList'));
-		$this->assertEquals($r->match('/news/12'), array('action' => 'NewsShow', 'id' => 12));
-		$this->assertEquals($r->match('/news/wtf'), null);
+		$this->assertEquals($r->match('/news'), array('action' => 'list'));
+		$this->assertEquals($r->match('/news/12'), array('action' => 'show', 'id' => 12));
 	}
 	
 	public function testCreateUrl() {
 		$r = new Johnny_Router();
-		$r->connect('/news', array('action' => 'NewsList'));
-		$r->connect('/news/:id', array('action' => 'NewsShow'));
+		$r->connect('/news', array('action' => 'list'));
+		$r->connect('/news/:id', array('action' => 'show'));
 
-		$this->assertEquals($r->url(array('action' => 'NewsList')), '/news');
-		$this->assertEquals($r->url(array('action' => 'NewsShow', 'id' => 12)), '/news/12');
-		$this->assertEquals($r->url(array('action' => 'NewsShow', 'wtf' => 37)), null);
+		$this->assertEquals($r->url(array('action' => 'list')), '/news');
+		$this->assertEquals($r->url(array('action' => 'show', 'id' => 12)), '/news/12');
+		$this->assertEquals($r->url(array('action' => 'show', 'wtf' => 37)), null);
+	}
+
+	public function testRouteWithRe() {
+		$r = new Johnny_Router();
+		$r->connect('/news', array('action' => 'list'));
+		$r->connect('/news/:id', array('action' => 'show', 'id' => '\d+'));
+		$r->connect('/news/:id/:slug', array('action' => 'show'));
+		$r->connect('/test/:arg/xx', array('arg' => '(a|b)'));
+		$r->connect('/test2/:arg/:arg2', array());
+
+		$this->assertEquals($r->match('/news'), array('action' => 'list'));
+		$this->assertEquals($r->match('/news/12'), array('action' => 'show', 'id' => 12));
+		$this->assertEquals($r->match('/news/wtf'), null);
+		$this->assertEquals($r->match('/news/12/test'), array('action' => 'show', 'id' => 12, 'slug' => 'test'));
+		$this->assertEquals($r->match('/test/a/xx'), array('arg' => 'a'));
+		$this->assertEquals($r->match('/test/c/xx'), null);
+		$this->assertEquals($r->match('/test2/a/b'), array('arg' => 'a', 'arg2' => 'b'));
 	}
 }
 
